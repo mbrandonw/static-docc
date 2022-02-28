4ec2277abe3afdcb856cf09e90f57b4921a189aa
Brandon Williams <mbrandonw@hey.com>
2022-02-28 08:52:23 -0800

    wip

---
+++ Sources/static-docc/static_docc.swift
@@ -0,0 +1,6 @@
+public struct static_docc {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/static-doccTests/static_doccTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import static_docc
+
+final class static_doccTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(static_docc().text, "Hello, World!")
+    }
+}
